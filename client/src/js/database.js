3376ad8c685149098a80869d88b52f6ba2942ea6
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-08-31 18:14:10 -0400

    fixed putDB bugs

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,19 +13,18 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) =>{
+export const putDb = async (content) =>{
 
   console.log('PUT to the database', content);
 
   const jateDB = await openDB('jate', 1);
   const tx = jateDB.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, todo: content });
+  const request = store.put({ id: 1, todo: content });
   const result = await request;
 
   console.log('🚀 - data saved to the database', result);
 
-  console.error('putDb not implemented');
 } 
 
 
@@ -43,10 +42,7 @@ export const getDb = async () => {
   const result = await request;
   
   console.log('result.value', result);
-  return result;
-
-
-  
+  return result.todo;
 } 
 
 initdb();
